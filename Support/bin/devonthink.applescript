4b38937dd3ac424c8ed2a7c12863f568382ec4c0
PuLLi <112799107+the-pulli@users.noreply.github.com>
2023-11-15 16:01:47 +0100

    refactor to external applescript file

---
+++ Support/bin/devonthink.applescript
@@ -0,0 +1,88 @@
+use AppleScript version "2.8"
+use framework "Foundation"
+use scripting additions
+
+property ca : a reference to current application
+property NSData : a reference to ca's NSData
+property NSDictionary : a reference to ca's NSDictionary
+property NSJSONSerialization : a reference to ca's NSJSONSerialization
+property NSString : a reference to ca's NSString
+property NSUTF8StringEncoding : a reference to 4
+
+on parseJSON(theJson)
+  set theJSONString to (NSString's stringWithString:theJson)
+  set JSONdata to (theJSONString's dataUsingEncoding:NSUTF8StringEncoding)
+  set [elements, E] to (NSJSONSerialization's JSONObjectWithData:JSONdata options:0 |error|:(reference))
+  if E ≠ missing value then error E
+  tell elements to if its isKindOfClass:NSDictionary then return it as record
+  elements as list
+end parseJSON
+
+on cleanDuplicateRecord(theRecord)
+  tell application id "DNtp"
+    set theDuplicates to duplicates of theRecord
+    set deleteDuplicateRecord to DEVONTHINK_DELETE_DUPLICATE_RECORD
+    set moveToTrash to DEVONTHINK_MOVE_TO_TRASH
+    set isDeleted to false
+    if deleteDuplicateRecord then
+      if (length of theDuplicates is greater than 0) then
+        if moveToTrash then
+          move record theRecord to (trash group of database of theRecord)
+        else
+          delete record theRecord
+        end if
+        set isDeleted to true
+      end if
+    end if
+  end tell
+  return isDeleted
+end cleanDuplicateRecord
+
+try
+    tell application id "DNtp"
+        set theAttachments to my parseJSON(DEVONTHINK_JSON)
+        repeat with theAttachment in theAttachments
+            set theRecord to import (devonthinkFilepath of theAttachment)
+
+            if theRecord is not missing value then
+              if (creation date of theRecord) is not missing value then
+                set theModificationDate to creation date of theRecord
+              else
+                set theModificationDate to "DEVONTHINK_DATE"
+              end if
+
+              set modification date of theRecord to theModificationDate
+
+              set the URL of theRecord to "DEVONTHINK_URL" # Allows it to be opened in MailMate using ⌃⌘U
+
+              try
+                # Shell script hack from Jim N. (DEVONtechnologies)
+                do shell script "tail -n 5 " & (quoted form of (path of theRecord as string)) & " | grep Encrypt"
+                set theEncryption to true
+              on error
+                set theEncryption to false
+              end try
+
+              if (type of theRecord is PDF document) and (word count of theRecord is 0) and (theEncryption is false) then
+                set ocrRecord to (ocr file path of theRecord) waiting for reply true
+                set modification date of ocrRecord to theModificationDate
+                set the URL of ocrRecord to "DEVONTHINK_URL" # Allows it to be opened in MailMate using ⌃⌘U
+                set theResult to delete record theRecord
+                my cleanDuplicateRecord(ocrRecord)
+              end if
+
+              set wasDeleted to my cleanDuplicateRecord(theRecord)
+              if wasDeleted is false then
+                display notification (devonthinkFilename of theAttachment) & " imported." with title "MailMate"
+              end if
+            else
+              display notification "File could not be imported." with title "MailMate"
+            end if
+        end repeat
+    end tell
+on error errMsg number eNum
+    tell application "System Events"
+        activate
+        display alert "DEVONthink: " & eNum message errMsg
+    end tell
+end try
--- Support/bin/files.rb
+++ Support/bin/files.rb
@@ -8,99 +8,29 @@ module DEVONthink
   # Handles adding files to DT
   class Files
     def initialize(attachments)
-      @attachments = attachments
+      @attachments = attachments.map do |hsh|
+        hsh.transform_keys { |k| k.to_s.include?("file") ? k.to_s.sub("file", "devonthinkFile").to_sym : k }
+      end
+      # inspect is necessary due to escaping double quotes for AppleScript
+      # TODO: possible bug, if filename contains double quotes
+      @json = JSON.generate(@attachments).inspect
+      @url = @attachments.first[:url]
+      @date = Date.new.strftime("%Y-%m-%d")
     end
 
     def add
-      # Convert Ruby Array with Hashes to an AppleScript Record String for the given files
-      substitution = {
-        "[" => "{",
-        "]" => "}",
-        "=>" => ":",
-        ", " => ",",
-        ":" => ""
+      map = {
+        "DEVONTHINK_DATE" => @date,
+        "DEVONTHINK_DELETE_DUPLICATE_RECORD" => Rule::DELETE_DUPLICATE_RECORD.to_s,
+        "DEVONTHINK_JSON" => @json,
+        "DEVONTHINK_MOVE_TO_TRASH" => Rule::MOVE_TO_TRASH.to_s,
+        "DEVONTHINK_URL" => @url
       }
-
-      attachments_record = @attachments.to_s.gsub(/[\[\]=>:\\]*(, )*/, substitution)
-                                       .gsub(/"(filepath|filename)"/, '\1')
-
-      url = @attachments.first[:url]
-      date = Date.new.strftime("%Y-%m-%d")
-
+      re = Regexp.new(map.keys.map { |x| Regexp.escape(x) }.join("|"))
       # Use AppleScript to communicate with DEVONthink
-      apple_script = <<~APS.strip
-        use scripting additions
+      apple_script = File.read("#{__dir__}/devonthink.applescript").gsub(re, map)
 
-        on cleanDuplicateRecord(theRecord)
-          tell application id "DNtp"
-            set theDuplicates to duplicates of theRecord
-            set deleteDuplicateRecord to #{Rule::DELETE_DUPLICATE_RECORD}
-            set moveToTrash to #{Rule::MOVE_TO_TRASH}
-            set isDeleted to false
-            if deleteDuplicateRecord then
-              if (length of theDuplicates is greater than 0) then
-                if moveToTrash then
-                  move record theRecord to (trash group of database of theRecord)
-                else
-                  delete record theRecord
-                end if
-                set isDeleted to true
-              end if
-            end if
-          end tell
-          return isDeleted
-        end cleanDuplicateRecord
-
-        try
-            tell application id "DNtp"
-                repeat with theAttachment in #{attachments_record}
-                    set theRecord to import (filepath of theAttachment)
-
-                    if theRecord is not missing value then
-                      if (creation date of theRecord) is not missing value then
-                        set theModificationDate to creation date of theRecord
-                      else
-                        set theModificationDate to "#{date}"
-                      end if
-
-                      set modification date of theRecord to theModificationDate
-
-                      set the URL of theRecord to "#{url}" # Allows it to be opened in MailMate using ⌃⌘U
-
-                      try
-                        # Shell script hack from Jim N. (DEVONtechnologies)
-                        do shell script "tail -n 5 " & (quoted form of (path of theRecord as string)) & " | grep Encrypt"
-                        set theEncryption to true
-                      on error
-                        set theEncryption to false
-                      end try
-
-                      if (type of theRecord is PDF document) and (word count of theRecord is 0) and (theEncryption is false) then
-                        set ocrRecord to (ocr file path of theRecord waiting for reply true)
-                        set modification date of ocrRecord to theModificationDate
-                        set the URL of ocrRecord to "#{url}" # Allows it to be opened in MailMate using ⌃⌘U
-                        set theResult to delete record theRecord
-                        my cleanDuplicateRecord(ocrRecord)
-                      end if
-
-                      set wasDeleted to my cleanDuplicateRecord(theRecord)
-                      if wasDeleted is false then
-                        display notification (fileName of theAttachment) & " imported." with title "MailMate"
-                      end if
-                    else
-                      display notification "File could not be imported." with title "MailMate"
-                    end if
-                end repeat
-            end tell
-        on error errMsg number eNum
-            tell application "System Events"
-                activate
-                display alert "DEVONthink: " & eNum message errMsg
-            end tell
-        end try
-      APS
-
-      system "osascript -e '#{apple_script}'"
+      exec "osascript", "-e", apple_script
     end
   end
 end
